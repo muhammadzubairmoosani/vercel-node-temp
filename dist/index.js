79ab830d42eb1a6e49da190ae053f7f9f9b98fcd
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2023-08-07 23:35:26 +0500

    connect mongoose

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,58 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
+app.use(express_1.default.json());
+app.listen(port, () => {
+    return console.log(`Server is listening on ${port}`);
+});
+const dbURL = "mongodb+srv://test_user:4NwlhrRrw7R9KrQU@cluster0.uso8mpp.mongodb.net/test";
+mongoose_1.default
+    .connect(dbURL)
+    .then(() => {
+    console.log("Connected to MongoDB");
+})
+    .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+});
+const userSchema = new mongoose_1.default.Schema({
+    username: String,
+    email: String,
+});
+const User = mongoose_1.default.model("User", userSchema);
 app.get("/", (_req, res) => {
     return res.send("Express Typescript on Vercel");
 });
 app.get("/ping", (_req, res) => {
     return res.send("pong 🏓");
 });
-app.listen(port, () => {
-    return console.log(`Server is listening on ${port}`);
-});
+app.post("/users", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const users = yield User.create(_req.body);
+    res.status(201).json({
+        status: "success",
+        users,
+    });
+}));
+app.get("/users", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const users = yield User.find();
+    res.status(200).json({
+        status: "success",
+        results: users.length,
+        users,
+    });
+}));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.listen(port, () => {
+  return console.log(`Server is listening on ${port}`);
+});
+
+const dbURL =
+  "mongodb+srv://test_user:4NwlhrRrw7R9KrQU@cluster0.uso8mpp.mongodb.net/test";
+
+mongoose
+  .connect(dbURL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error: any) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+});
+
+const User = mongoose.model("User", userSchema);
 
 app.get("/", (_req: Request, res: Response) => {
   return res.send("Express Typescript on Vercel");
@@ -11,6 +36,21 @@ app.get("/ping", (_req: Request, res: Response) => {
   return res.send("pong 🏓");
 });
 
-app.listen(port, () => {
-  return console.log(`Server is listening on ${port}`);
+app.post("/users", async (_req: Request, res: Response) => {
+  const users = await User.create(_req.body);
+
+  res.status(201).json({
+    status: "success",
+    users,
+  });
+});
+
+app.get("/users", async (_req: Request, res: Response) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: "success",
+    results: users.length,
+    users,
+  });
 });
